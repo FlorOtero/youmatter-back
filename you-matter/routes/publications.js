274dba37eed1e5bfc2b73baeb9a1143a4e0bfc2a
Javier Castro <javier.castro@mercadolibre.com>
2018-11-11 03:06:42 -0300

    add get publication by id

--- you-matter/routes/publications.js
+++ you-matter/routes/publications.js
@@ -3,4 +3,20 @@ const { close, driver } = require('./../neo4j');
 var express = require('express');
 var router = express.Router();
 
+router.get('/:id', function (req, res) {
+  const session = driver.session();
+  session.run(`MATCH (p:Publication) WHERE id(p) = ${req.params.id} RETURN p`)
+    .then(function (result) {
+      let nodes = [];
+      result.records.forEach(function (record) {
+        nodes.push(record.get(0).properties);
+      })
+      session.close();
+      res.send(nodes);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
 module.exports = router;
