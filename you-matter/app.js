e2e4af4c5e833ecebd1add81a82c2c4e57bb0d3e
Erika Nuñez <erikannunez@gmail.com>
2018-11-10 23:05:13 -0300

    fix route

--- you-matter/app.js
+++ you-matter/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var publicationsRouter = require('./routes/users');
+var publicationsRouter = require('./routes/publications');
 
 var app = express();
 
